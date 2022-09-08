a4ad495f8d03a87cb7b44221c990c3a5240d267f
vikas ranjan <vikasranjan786.vr@gmail.com>
2022-09-08 23:59:07 +0530

    asd

---
+++ src/Atm.java
@@ -0,0 +1,18 @@
+public class A.tm {
+    private double balance;
+    private double depositAmount;
+    private double withdrawAmount;
+
+    //default constructor
+    public Atm(){
+
+    }
+
+    //getter setter
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance=balance;
+    }
+}
